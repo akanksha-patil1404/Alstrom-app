ddfa242343e037285ea64bcb03d72040506f4887
akanksha-patil1404 <mmkapatil5555@gmail.com>
2024-07-12 12:38:58 +0530

    commit for tests

--- src/test/java/com/training/bms/AlstromAppApplicationTests.java
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -1,13 +1,18 @@
 package com.training.bms;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AlstromAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		assertEquals(190,200);
 	}
 
 }
